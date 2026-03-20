86065971a250a2e20e3394303e7f0fc4c488a1cd
marvin2k0 <25713103+marvin2k0@users.noreply.github.com>
2026-03-20 13:30:36 +0100

    Add search page with component, styles, and unit test

---
+++ frontend/src/app/pages/search/search.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import {MatIcon} from '@angular/material/icon';
+
+@Component({
+  selector: 'app-search',
+  imports: [
+    MatIcon,
+  ],
+  templateUrl: './search.html',
+  styleUrl: './search.css',
+})
+export class Search {}
