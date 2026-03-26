9025393a2bc018815e29973c0f457a87eba9a002
marvin2k0 <25713103+marvin2k0@users.noreply.github.com>
2026-03-26 18:27:57 +0100

    Update API URL and integrate environment-based configuration in course service

--- frontend/src/app/course/course-service.ts
+++ frontend/src/app/course/course-service.ts
@@ -1,14 +1,15 @@
 import {Injectable} from '@angular/core';
 import {httpResource} from '@angular/common/http';
 import {Course} from './course-model';
+import { AppConfig as config } from '../../environments/app-config';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CourseService {
-  private readonly baseUrl = 'http://localhost:8080/v1/course/'
+  private readonly baseUrl = config.apiUrl
 
   getCourse(id: string) {
-    return httpResource<Course>(() => `${this.baseUrl}${id}`);
+    return httpResource<Course>(() => `${this.baseUrl}/v1/course/${id}`);
   }
 }
